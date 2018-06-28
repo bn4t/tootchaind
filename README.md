e3fafbddd341b449904cb7ec28f97c4f79122d05
bn4t <info@apply55gx.com>
2018-06-28 14:03:42 +0200

    rewrite done

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ## Block
 
 ### Blockhash
+
+The blockhash is calculated using following method:
 ````
 blockhash = sha256([NONCE][TIMESTAMP][PREV_BLOCKHASH][DATA])
 ````
@@ -14,3 +16,21 @@ Every blockhash starts with `7007` (`TOOT` written in numbers)
 
 The genesis blockhash is `0000000000000000000000000000000000000000000000000000000000000000`
 
+### Structure
+
+Structure of a block
+
+````
+{
+  "height": 123,
+  "timestamp": 1530168776,
+  "transactions": [
+    {
+        "sender": "bn4t@toot.cafe",
+        "data": "Hello World!"
+    }
+  ],
+  "nonce": 1234,
+  "previous_hash": "7fdcde7c3f0db44f3f7ff62b0d0ecc828536b8576f48ab690e6a49adaecc5105"
+}
+````
--- tootchaind.py
+++ tootchaind.py
@@ -1,15 +1,16 @@
 import hashlib
 import json
-import time
-from time import time
 import os
 import re
-from mastodon import Mastodon
-from flask import Flask, jsonify, request
+import time
+from time import time
+
 from bs4 import BeautifulSoup
+from mastodon import Mastodon
 
 
 class Tootchaind:
+
     def __init__(self):
         self.pending_tx = []
         self.mastodon = None
@@ -17,11 +18,25 @@ class Tootchaind:
 
         self.initialize_mastodon()
 
-    def gen_genesis_block(self, nonce):
-        # create the block
+        # generate genesis block if there is no previous one
+        timeline = self.mastodon.timeline_home()
+        # if len(timeline) < 1:
+        #    self.gen_genesis_block()
+
+        self.gen_genesis_block()
+
+        # retrieve txs and create a block
+        self.retrieve_tx()
+        self.create_block()
+
+    # create the genesis block
+    def gen_genesis_block(self):
+
+        nonce = self.proof_of_work('0000000000000000000000000000000000000000000000000000000000000000', [])
+
         block = {
-            'index': 1,
-            'timestamp': int(time.time()),
+            'height': 0,
+            'timestamp': int(time()),
             'transactions': [],
             'nonce': nonce,
             'previous_hash': '0000000000000000000000000000000000000000000000000000000000000000',
@@ -57,3 +72,131 @@ class Tootchaind:
             access_token='user_cred.txt',
             api_base_url=self.instance_url
         )
+
+    # create new transaction
+    def new_transaction(self, sender, data):
+
+        # Enforce 50 character limit so blocks don't exceed the 500 characters which fit into a toot
+        if len(data) > 50:
+            return 1
+
+        self.pending_tx.append({
+            'sender': sender,
+            'data': data,
+        })
+
+    # create a new block
+    def create_block(self):
+
+        to_include_transactions = []
+
+        # Choose transactions to include in block
+        for _ in range(3):
+
+            # check if array is empty
+            if self.pending_tx:
+                # add transaction to block and remove it from the array
+                to_include_transactions.append(self.pending_tx[0])
+                self.pending_tx.pop(0)
+
+        previous_hash = self.last_hash()
+        nonce = self.proof_of_work(previous_hash, to_include_transactions)
+
+        # create the block
+        block = {
+            'height': self.block_height() + 1,
+            'timestamp': int(time()),
+            'transactions': to_include_transactions,
+            'nonce': nonce,
+            'previous_hash': previous_hash,
+        }
+
+        # Toot the formatted block
+        self.mastodon.status_post(json.dumps(block), visibility='unlisted')
+
+    # calculates the nonce
+    def proof_of_work(self, previous_hash, data):
+
+        nonce = 0
+        while self.valid_proof(nonce, previous_hash, data) is False:
+            nonce += 1
+
+        return nonce
+
+    def retrieve_tx(self):
+        json_notif = self.mastodon.notifications()
+
+        # iterate through notifications
+        for i, current_notif in enumerate(json_notif):
+            # check if the notification type is mention
+            if current_notif['type'] == 'mention':
+
+                # fill in variables
+                username = str(current_notif['account']['username'])
+                content = str(current_notif['status']['content'])
+
+                # check if the correct command is used
+                if content.startswith(
+                        '<p><span class="h-card"><a href="https://toot.cafe/@tootchain_test" class="u-url '
+                        'mention">@<span>tootchain_test</span></a></span> !tx '):
+                    # use substring of content without the command
+                    data = content[132:]
+                    clean_data = self.clean_html(data)
+
+                    self.new_transaction(username, clean_data)
+                    print("processed tx")
+
+        # clear notifications
+        self.mastodon.notifications_clear()
+
+    #####################################
+    #               Utils               #
+    #####################################
+
+    def last_block(self):
+        json_tl = self.mastodon.timeline_home(limit=1)
+
+        return json.loads(self.unescape_text(
+            self.clean_html(json_tl[0]['content'])))  # returns the latest block from the timeline formatted as json
+
+    @staticmethod
+    def unescape_text(text):
+
+        soup = BeautifulSoup(text, "html.parser")
+        return soup.text
+
+    @staticmethod
+    def clean_html(raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+    def block_height(self):
+        last_block_json = self.last_block()
+        return int(last_block_json['height'])
+
+    def last_hash(self):
+        block = self.last_block()
+        nonce = block['nonce']
+        timestamp = block['timestamp']
+        previous_blockhash = block['previous_hash']
+        data = block['transactions']
+
+        blockdata = f'{nonce}{timestamp}{previous_blockhash}{data}'.encode()
+        blockhash = hashlib.sha256(blockdata).hexdigest()
+
+        return blockhash
+
+    @staticmethod
+    def valid_proof(nonce, previous_blockhash, data):
+        timestamp = int(time())
+
+        guess = f'{nonce}{timestamp}{previous_blockhash}{data}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+
+        return guess_hash[:4] == "7007"
+
+
+blockchain = Tootchaind()
+
+blockchain.__init__()
