d5fc93fc9be5cc6b414203f574d36aa2b3f08479
bn4t <info@apply55gx.com>
2018-07-11 08:37:47 +0000

    rename variables

--- verify.py
+++ verify.py
@@ -36,20 +36,16 @@ class verify(object):
         last_block_data = json.dumps({"height":last_block_height, "timestamp":last_block_timestamp, "transactions":last_block_transactions, "nonce":last_block_nonce,
             "previous_hash":last_block_previous_hash}, sort_keys=True).encode()
         
-        print(last_block_data)
-
         last_hash = hashlib.sha256(last_block_data).hexdigest()
-        
-        
 
-        guess = f'{nonce}{timestamp}{previous_hash}{data}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
+        block = f'{nonce}{timestamp}{previous_hash}{data}'.encode()
+        block_hash = hashlib.sha256(block).hexdigest()
 
         # Debug
-        if guess_hash[:4] == "7007":
-            print("Block is valid! ( blockhash: " + guess_hash + " | nonce: " + str(nonce) + " | time: " + str(timestamp) + " | last_hash: " + last_hash + " | data: " + str(data))
+        if block_hash[:4] == "7007":
+            print("Block is valid! ( blockhash: " + block_hash + " | nonce: " + str(nonce) + " | time: " + str(timestamp) + " | last_hash: " + last_hash + " | data: " + str(data))
         else:
-            print("Block is invalid! ( blockhash: " + guess_hash + " | nonce: " + str(nonce) + " | time: " + str(timestamp) + " | last_hash: " + last_hash + " | data: " + str(data))
+            print("Block is invalid! ( blockhash: " + block_hash + " | nonce: " + str(nonce) + " | time: " + str(timestamp) + " | last_hash: " + last_hash + " | data: " + str(data))
     
 
 verifyc = verify()
